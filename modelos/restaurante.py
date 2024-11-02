c97ffc3253817ddd5a24012fb215f9ac343733dd
Ewerton Victor <93969276+EwertonVictor@users.noreply.github.com>
2024-11-02 00:41:54 -0300

    Implementando o codigo

---
+++ modelos/restaurante.py
@@ -0,0 +1,30 @@
+class Restaurante:
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__ (self):
+        return f'{self._nome} | {self._categoria}'
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '✅' if self._ativo else '🚫'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+
+restaurante_praca = Restaurante('praça','Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza Express','Italiana')
+
+Restaurante.listar_restaurantes()
